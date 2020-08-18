f96c26754c976b068d2c98fedd79eb3f7f577918
Ömer Faruk Gürbüz <omer.gurbuz@kletech.com>
2020-08-18 16:15:41 +0300

    #2 Component defined above npm

--- input-tag-component/README.md
+++ input-tag-component/README.md
@@ -1,12 +1,19 @@
-# input-tag-component
+# Input Tag Component
 
-> A Vue.js project
+> A Vue.js project npm package use
+
+# Screenshot
+![op](https://user-images.githubusercontent.com/38696355/90517095-9d011480-e16d-11ea-89ba-48b9813d1e74.png)
+
 
 ## Build Setup
 
 ``` bash
 # install dependencies
 npm install
+
+# install component - my package
+npm i npm-tagtt
 
 # serve with hot reload at localhost:8080
 npm run dev
--- input-tag-component/src/App.vue
+++ input-tag-component/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="container">
-    <app-tags v-model="tags" color="success"/> <!-- :value="tags" -->
-    <app-tags v-model="tags2" color="danger"/>
-    <app-tags v-model="tags2" color="info"/>
-    <app-tags v-model="tags2" color="primary"/>
-    <app-tags v-model="tags2" color="warning"/>
-    <app-tags v-model="tags2" color="secondary"/>
+    <vtag v-model="tags" color="success"/> <!-- :value="tags" -->
+    <vtag v-model="tags2" color="danger"/>
+    <vtag v-model="tags2" color="info"/>
+    <vtag v-model="tags2" color="primary"/>
+    <vtag v-model="tags2" color="warning"/>
+    <vtag v-model="tags2" color="secondary"/>
   </div>
 </template>
 <script>
-  import Tags from "./components/Tags"
+
   export default {
-    components : {
-      appTags : Tags
-    },
+
     data(){
       return {
         tags : "test1,test2,test3",
